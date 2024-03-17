5d826963d8b4f2ca87b4e096b19b6bddc306f84b
MishaTarasenko <127847522+MishaTarasenko@users.noreply.github.com>
2024-03-17 14:38:11 +0200

    add docstrings to input and output functions

--- project_template/app/io/input.py
+++ project_template/app/io/input.py
@@ -1,10 +1,52 @@
 def input_text_from_console():
+    """
+    Get console input and return entered string
+
+    Examples:
+        >>> input_text_from_console()
+        Hello World
+
+    Returns:
+        str: entered string
+    """
     pass
 
 
 def input_text_from_file(file_path):
+    """
+        Read from a file using built-in Python capabilities
+
+        Examples:
+            >>> input_text_from_console(../data/test.txt)
+            Text from file
+
+        Args:
+            file_path (str): path to file
+
+        Returns:
+            str: content of the file
+
+        Raises:
+            FileNotFoundError: if file does not exists
+    """
     pass
 
 
 def input_text_with_pandas(file_path):
+    """
+        Read from a file using the pandas library
+
+        Examples:
+            >>> input_text_with_pandas(../data/test.txt)
+            DataFrame of file
+
+        Args:
+            file_path (str): path to file
+
+        Returns:
+            DataFrame: Dataframe of file
+        Raises:
+            FileNotFoundError: if file does not exists
+            ValueError: If the file format is invalid or cannot be parsed as a DataFrame
+    """
     pass
--- project_template/app/io/output.py
+++ project_template/app/io/output.py
@@ -1,11 +1,49 @@
-def output_to_console():
+def output_to_console(text):
+    """
+        Print text to console
+
+        Examples:
+            >>> output_to_console("Hello world")
+
+        Args:
+            text (str): Text printed to console
+
+        Returns:
+            None
+    """
     pass
 
 
 def output_to_file(file_path, data):
+    """
+        Writes text to a file using the built-in Python capabilities
+
+        Examples:
+            >>> output_to_file(../data/test.txt, "Hello world")
+
+        Args:
+            file_path (str): path to file to write to
+            data (str): Text printed to file
+
+        Returns:
+            None
+    """
     pass
 
 
 def output_to_file_with_pandas(file_path, data):
+    """
+            Writes text to a file using the pandas library
+
+            Examples:
+                >>> output_to_file_with_pandas(../data/test.txt, "Hello world")
+
+            Args:
+                file_path (str): path to file to write to
+                data (str): Text printed to file
+
+            Returns:
+                None
+        """
     pass
 
